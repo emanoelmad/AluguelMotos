f0b6be31052d01d367c2ec26ed9da09c304810a7
Emanoel <josemanoel.am@gmail.com>
2025-08-25 19:51:44 -0300

    Ajuste nas entidades

--- AluguelMotos.Domain/Entities/Courier.cs
+++ AluguelMotos.Domain/Entities/Courier.cs
@@ -6,14 +6,14 @@ using System.Threading.Tasks;
 
 namespace AluguelMotos.Domain.Entities
 {
-public class Courier
-{
-    public Guid Id { get; set; }
-    public required string Name { get; set; }
-    public required string Cnpj { get; set; }
-    public DateTime BirthDate { get; set; }
-    public required string CnhNumber { get; set; }
-    public required string CnhType { get; set; } // A, B, A+B
-    public string? CnhImagePath { get; set; }
-}
+    public class Courier
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+        public required string Cnpj { get; set; }
+        public DateTime BirthDate { get; set; }
+        public required string CnhNumber { get; set; }
+        public required string CnhType { get; set; } // A, B, A+B
+        public string? CnhImagePath { get; set; }
+    }
 }
